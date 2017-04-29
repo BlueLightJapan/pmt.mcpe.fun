291a3b3b939d1fbaab989fa954e5a38ab2e04956
haniokasai <htek@haniokasai.com>
2017-04-29 19:40:46 +0900

    phar maker

--- phar.php
+++ phar.php
@@ -8,7 +8,7 @@ textdomain(PACKAGE);
 ?>
 <html>
 <head>
-	<title><?php echo _('Create phar'); ?></title>
+	<title><?php echo _('Pharの作成'); ?></title>
 	<link rel="stylesheet" href="/css/main.css" />
 	<script>
 	function fillPmStub(){
@@ -17,45 +17,44 @@ textdomain(PACKAGE);
 	</script>
 </head>
 <body>
-	<h1><?php echo _('Phar maker'); ?></h1>
-	<h3><?php echo _('How to use'); ?></h3>
+	<h1><?php echo _('Pharメーカー'); ?></h1>
+	<h3><?php echo _('使い方'); ?></h3>
 	<ol>
-		<li><?php echo _('Write your plugin, of course :D'); ?></li>
-		<li><?php echo _('Put your files in the correct structure (according to namespaces, etc.)'); ?></li>
-		<li><?php echo _('Create a ZIP file and throw your structure into it. You can put it anywhere inside the ZIP file, but only the folder with plugin.yml (and subfolders) will be included.'); ?></li>
-		<li><?php echo _('Upload that file below :)'); ?></li>
+		<li><?php echo _('もちろん、プラグインのコードそのものは書いてください　:D'); ?></li>
+		<li><?php echo _('正しい形（namespace)などのファイルやフォルダを用意してくださ'); ?></li>
+		<li><?php echo _('zipで圧縮してください。 ZIPファイルのどこにでもファイルを置けますが, plugin.ymlとsrcフォルダとその中身は正しく入れてください。'); ?></li>
+		<li><?php echo _('そｒをアップロードしてください :)'); ?></li>
 	</ol>
 	<form method="post" action="/phar-result.php" enctype="multipart/form-data">
 		<p><input type="file" name="file"></p>
-		<p><?php echo _('Stub (Leave as default unless you know what it is):'); ?>
+		<p><?php echo _('スタブ (知らない設定はそのままにしてください):'); ?>
 			<?php
 			echo '<input type="text" name="stub" value="';
 			echo '<?php __HALT_COMPILER();';
 			echo '" size="100" id="stubInput">';
 			?>
-			<button onclick="fillPmStub(); return false;"><?php echo _('Use the PocketMine-MP.phar stub'); ?></button>
+			<button onclick="fillPmStub(); return false;"><?php echo _('PocketMine-MP.pharのスタブを使うか'); ?></button>
 		</p>
-		<p><?php echo _('Tune the plugin using the following methods:'); ?> <br>
+		<p><?php echo _('下のプラグイン調整機能を使うか:'); ?> <br>
 			<input type="checkbox" name="tune_top_namespace_optimization">
-				<?php echo _('Optimize constant references by adding a <code>\</code> prefix to indicate that it is a top namespace reference.'); ?><br>
-			<input type="checkbox" name="tune_obfuscation"> <?php echo _('Obfuscate code'); ?>
+				<?php echo _('定数参照を最適化するには、<code>\</code> 接頭語を追加して、それが最上位名前空間参照であ​ることを示します。'); ?><br>
+			<input type="checkbox" name="tune_obfuscation"> <?php echo _('コードの難読化（BluelightやGenisysなどのプラグイン開発者はこれを使ってはいけない可能性があります'); ?>
 		</p>
-		<p><font color="#8b0000"><?php echo _('Warning: Using any of the tunes may stripe out all the comments in your code <em>except</em> PHP doc comments or line comments.'); ?></font></p>
+		<p><font color="#8b0000"><?php echo _('注意: これらの機能は、php内のコメントを消してしまう恐れがあります'); ?></font></p>
 		<p>
 			<?php echo _('Carry out the following inspections too:'); ?> <br>
-			<input type="checkbox" name="inspection_classpath"> <?php echo _('Check classpath'); ?><br>
-			<input type="checkbox" name="inspection_bad_practice"> <?php echo _('Scan for bad practice'); ?><br>
-			<input type="checkbox" name="inspection_lint"> <?php echo _('Syntax error lint scan'); ?>
+			<input type="checkbox" name="inspection_classpath"> <?php echo _('クラスパスのチェック'); ?><br>
+			<input type="checkbox" name="inspection_bad_practice"> <?php echo _('悪い実装の調査 '); ?><br>
+			<input type="checkbox" name="inspection_lint"> <?php echo _('シンタックスエラーの調査'); ?>
 		</p>
-		<p><input type="submit" value="<?php echo _('create phar'); ?>"></p>
+		<p><input type="submit" value="<?php echo _('pharの作成'); ?>"></p>
 	</form>
-	<p><?php echo _('New: use the frames page <a href="pm.php" target="_parent">here</a> if you are not already using.'); ?></p>
+	<p><?php echo _('New: フレームページを使ってください <a href="pm.php" target="_parent">ここ</a> (もし使っていなければ）'); ?></p>
 <pre>
-	<?php echo _("Disclaimer:
-	This service is provided absolutely free of charge and is not guaranteed always available.
-	This page (phar.php) extracts ZIP files uploaded by users into a location in the server's filesystem that is not available to users, executes tuning and packs the result into a *.phar file that is available for public download. This process is 100% automated.
-	This website (http://pmt.mcpe.fun) is in no ways affiliated with PocketMine-MP (http://pocketmine.net), an open-source project developed by the PocketMine Team, or Minecraft: PE, a game software developed by Mojang.
-	We (owner of this website) are not to be held responsible for any acts related to copyright breaches and other illegal acts. All contents in the downloaded phar are either generated using the uploaded file or the software used for this website."); ?>
+	<?php echo _("免責事項：
+	このサービスは無料で提供されており、常に利用可能であることが保証されていません。
+	作業の過程で、 ファイルは、 （ウェブサイトからアクセス可能でないであろう）サーバのファイルシステム上のファイルに抽出し、一定期間保管します。
+	私たちは（このウェブサイトの所有者）は、著作権侵害やその他の違法行為に関連する行為のための責任を負いません。"); ?>
 </pre>
 </body>
 </html>
